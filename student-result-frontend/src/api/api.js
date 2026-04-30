2277e1335809636f0da4ad934d1dea932af2d855
Kathir-vel0 <kvel28328@gmail.com>
2026-04-30 12:24:16 +0530

    connected frontend to live backend

--- student-result-frontend/src/api/api.js
+++ student-result-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`
+  baseURL: "https://student-result-system-10c4.onrender.com/api"
 });
 
 export default API;
